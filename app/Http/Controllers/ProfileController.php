13719b7a933ffbb00f7d5276c6d8271bcc8f2689
Mingzhen0814 <fcum1102930@gmail.com>
2024-02-16 13:59:33 +0800

    上傳大頭貼並顯示

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -26,7 +26,16 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        $request->user()->fill($request->validated()); 
+        // 判斷使用者是否有上傳圖片，並做對應處理(是: 傳入圖片 / 否: 保留原有的圖片)
+        $validated = $request->validated();
+        if (isset($request->image)){
+            $data = $request->image->get();
+            $mime_type = $request->image->getMimeType(); // 假設使用者上傳的是jpg就會回傳"image/jpeg"
+            $imageData = base64_encode($data);
+            $src = "data: $mime_type;base64,$imageData"; // <img>要求的格式data: $mime_type;base64,(一定要這樣打)
+            $validated["image"] = $src; // 補上rules的JSON，不寫在rules是不要寫死，使其能動態上傳
+        }
+        $request->user()->fill($validated); 
         // validate()會以JSON回傳requests的rules()的key
         // fill會參考models的$fillable對應到JSON的值就會填入到資料庫
         if ($request->user()->isDirty('email')) {
--- resources/views/profile/partials/update-image-form.blade.php
+++ resources/views/profile/partials/update-image-form.blade.php
@@ -1,3 +1,11 @@
+<style>
+.profilephoto {
+    object-fit: cover;
+    object-position: center;
+    height: 200px;
+    width: 200px;
+}
+</style>
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
@@ -8,12 +16,14 @@
             {{ __("Update your head image.") }}
         </p>
     </header>
+    <!-- 將圖片顯示至前端 -->
+    <img src="{{$user->image}}" class="profilephoto rounded-lg">
 
     <form id="send-verification" method="post" action="{{ route('verification.send') }}">
         @csrf
     </form>
-
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <!-- enctype="multipart/form-data" 上傳檔案 -->
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6" enctype="multipart/form-data">
         @csrf
         @method('patch')
 
@@ -22,6 +32,11 @@
             <x-text-input id="image" name="image" type="file" class="mt-1 block w-full"/>
             <x-input-error class="mt-2" :messages="$errors->get('image')" />
         </div>
+
+        <!-- 使用type="hidden"以免被擋住 -->
+        <input type="hidden" name="name" value="{{$user->name}}">
+        <input type="hidden" name="phone" value="{{$user->phone}}">
+        <input type="hidden" name="email" value="{{$user->email}}">
         
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
