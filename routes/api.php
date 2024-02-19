8b8cd1f3e9eecfffba04f9116cc6a8d03fdd59bf
Mingzhen0814 <fcum1102930@gmail.com>
2024-02-19 16:27:19 +0800

    RESTful & ajax 0219

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/',function(Request $request) {
+    $uid = $request->uid;
+    $users = DB::select('select uid, ifnull(cname,"noname") as cname, ifnull(birth,"0000-00-00 00:00:00") as birth, ifnull(PWD,"lost") as PWD from UserInfo'); // 陣列
+    // $jsonString = $users->toJson(JSON_UNESCAPED_UNICODE);
+    return response($users); // JSON格式 (若使用model要手動轉json)
+        // ->header("content-type","application/json")
+        // ->header("charset","utf-8"); // 編碼格式
+});
+
+Route::post('/',function(Request $request) {
+    $cname = $request->cname;
+    $users = DB::select("select * from UserInfo where cname = '{$cname}'"); // 陣列
+    return response($users); // JSON格式 (若使用model要手動轉json)
 });
